c9d9a4ef13d89bc25fb48d24da3012e0baf19bae
FlagiH <neftimognat@mail.ru>
2023-02-03 21:26:56 +0400

    nafig vse ya otdihat!

--- src/js/index.js
+++ src/js/index.js
@@ -23,11 +23,11 @@ readMoreButton.addEventListener('click', function () {
 
   if (cuttedBlock.classList.contains('brands__block--cut')) {
     cuttedBlock.classList.remove('brands__block--cut');
-    readMoreButton.classList.add('brands__button-read-more--rotated')
+    readMoreButton.classList.add('brands__button-read-more--rotated');
     readMoreButton.textContent = 'Скрыть'; 
   } else {
     cuttedBlock.classList.add('brands__block--cut');
-    readMoreButton.classList.remove('brands__button-read-more--rotated')
+    readMoreButton.classList.remove('brands__button-read-more--rotated');
     readMoreButton.textContent = 'Показать все';
   }
 })
@@ -39,15 +39,29 @@ readMoreButtonTechnics.addEventListener('click', function () {
 
   if (cuttedBlockTechnics.classList.contains('technics__block--cut')) {
     cuttedBlockTechnics.classList.remove('technics__block--cut');
-    readMoreButtonTechnics.classList.add('technics__button-read-more--rotated')
+    readMoreButtonTechnics.classList.add('technics__button-read-more--rotated');
     readMoreButtonTechnics.textContent = 'Скрыть'; 
   } else {
     cuttedBlockTechnics.classList.add('technics__block--cut');
-    readMoreButtonTechnics.classList.remove('technics__button-read-more--rotated')
+    readMoreButtonTechnics.classList.remove('technics__button-read-more--rotated');
     readMoreButtonTechnics.textContent = 'Показать все';
   }
 })
 
 
+const readMoreSectionButton = document.querySelector('.section__button');
+const cuttedBlockSectionButton = document.querySelector('.section__text--hidden');
 
+readMoreSectionButton.addEventListener('click', function () {
+
+  if (cuttedBlockSectionButton.classList.contains('section__text--hidden')) {
+    cuttedBlockSectionButton.classList.remove('section__text--hidden');
+    readMoreSectionButton.classList.add('section__button--rotated');
+    readMoreSectionButton.textContent = 'Скрыть';
+  } else {
+    cuttedBlockSectionButton.classList.add('section__text--hidden');
+    readMoreSectionButton.classList.remove('section__button--rotated');
+    readMoreSectionButton.textContent = 'Показать все';
+  }
+})
 console.log(cuttedBlock);
